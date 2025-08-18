457feff05bdc5deb88fa8ff4da2195937cb40107
miguelmpp <miguelmpp12@gmail.com>
2025-08-18 11:24:06 -0300

    adicionando exercicio

--- StartCS/Program.cs
+++ StartCS/Program.cs
@@ -6,7 +6,29 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
+            int idade1, idade2;
+
+            
+            Console.WriteLine("Insira a idade do primeiro aluno:");
+            idade1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira a idade do segundo aluno:");
+            idade2 = int.Parse(Console.ReadLine());
+
+
+            if (idade1 > idade2)
+            {
+                Console.WriteLine("O primeiro aluno é o mais velho e o segundo aluno é o mais novo.");
+            }
+            else if (idade2 > idade1)
+            {
+                Console.WriteLine("O segundo aluno é o mais velho e o primeiro aluno é o mais novo.");
+            }
+            else
+            {
+                Console.WriteLine("Ambos os alunos têm a mesma idade.");
+            }
         }
     }
 }
